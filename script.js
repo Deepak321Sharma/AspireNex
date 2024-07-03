c5f94c11e160c6c319d0b573c638c75dbc8ef924
Deepak321Sharma <140793630+Deepak321Sharma@users.noreply.github.com>
2024-07-03 19:45:42 +0530

    [ADD]: Checkpoint 3-7-24

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = documnent.querySelector(".menu-links");
+    const icon = documnent.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
