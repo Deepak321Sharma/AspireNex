4d095ebc291dcc88cd98286ec6e589b47ad772e1
Deepak321Sharma <140793630+Deepak321Sharma@users.noreply.github.com>
2024-07-06 00:00:55 +0530

    script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    const menu = documnent.querySelector(".menu-links");
-    const icon = documnent.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
